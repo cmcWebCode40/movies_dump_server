6691d4b30ce35a3383ef5efde6e2b3f15df49c0c
cmcWebCode40 <infinity_michael@yahoo.com>
2024-06-05 22:59:12 +0100

    feat: project setup

---
+++ src/main/java/com/movies_api/movies_api/MovieService.java
@@ -0,0 +1,20 @@
+package com.movies_api.movies_api;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private  MovieRepository movieRepository;
+    public List<Movie> movies(){
+      return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMoviesByImdb(imdbId);
+    }
+}
---
+++ src/main/java/com/movies_api/movies_api/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.movies_api.movies_api;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
